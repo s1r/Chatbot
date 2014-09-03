5e75196be6152361786f095151f11481451a8b13
s1r@ <s1r@>
2014-09-03 22:33:20 +0900

    sh

--- Chatbot/runapp.sh
+++ Chatbot/runapp.sh
@@ -2,5 +2,9 @@
 
 CLASSPATH=./lib/derby.jar:./lib/twitter4j-core-4.0.2.jar:./lib/jargp.jar:./lib/lucene-gosen-4.5.1-ipadic.jar:.
 
+cp -p twitter4j.properties twitter4j.properties.b
+cp -p twitter4j.properties.bk twitter4j.properties
+
 java -classpath $CLASSPATH Main
 
+mv twitter4j.properties.b twitter4j.properties
